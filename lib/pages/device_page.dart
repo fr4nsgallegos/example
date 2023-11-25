6ac69e5484618f2bab7fd4c85e0880425b631725
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-24 22:02:05 -0500

    reto estacionamiento de avion

--- lib/pages/device_page.dart
+++ lib/pages/device_page.dart
@@ -5,6 +5,32 @@ class DevicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              width: 200,
+              height: 200,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                // color: Colors.red,
+                // borderRadius: BorderRadius.circular(100),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.orange,
+                  width: 10,
+                ),
+              ),
+              child: Text(
+                "H",
+                style: TextStyle(color: Colors.orange, fontSize: 100),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
