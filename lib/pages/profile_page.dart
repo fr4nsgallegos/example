85de00429b20315dc7c62078e001ba6ee9b76da2
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-26 12:22:46 -0500

    profile page avance

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -7,30 +7,79 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Ejemplo de stack"),
-        ),
-        body: Stack(
+        backgroundColor: Color(0xff181818),
+        body: Column(
           children: [
             Container(
-              width: MediaQuery.of(context).size.width,
-              height: double.infinity,
-              color: Colors.cyan,
-            ),
-            Positioned(
-              top: 50,
-              left: 50,
-              child: Text(
-                "Hola pertenezco a un stack",
-                style: TextStyle(color: Colors.white, fontSize: 20),
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
               ),
-            ),
-            Positioned(
-              top: 50,
-              left: 50,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Presionar"),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://img.freepik.com/foto-gratis/chico-guapo-seguro-posando-contra-pared-blanca_176420-32936.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1699401600&semt=ais",
+                    ),
+                    radius: 60,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Federico Perez",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Developer",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Text("Folowers"),
+                                  Text("25"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text("Likes"),
+                                  Text("250"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text("Rating"),
+                                  Text("9.8"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ],
