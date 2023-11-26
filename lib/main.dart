911e805015092dc2769aace2b293a950e168b159
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-26 11:06:59 -0500

    init profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:example/pages/future_page.dart';
 import 'package:example/pages/home_page.dart';
 import 'package:example/pages/init_page.dart';
 import 'package:example/pages/main_page.dart';
+import 'package:example/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: FuturePage(),
+      home: ProfilePage(),
       debugShowCheckedModeBanner: false,
     ),
   );
