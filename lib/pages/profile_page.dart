0955e9682c317c8057ec74f556258668961617b2
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-26 11:21:45 -0500

    stack

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -7,8 +7,34 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
-        body: Column(),
+        appBar: AppBar(
+          title: Text("Ejemplo de stack"),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: double.infinity,
+              color: Colors.cyan,
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Text(
+                "Hola pertenezco a un stack",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Presionar"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
