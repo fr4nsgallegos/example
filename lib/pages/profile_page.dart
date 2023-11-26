de1a437a365189eaf6a10cb8063703bc5703492c
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-11-26 12:44:52 -0500

    services init

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -22,52 +22,87 @@ class ProfilePage extends StatelessWidget {
                   bottomLeft: Radius.circular(50),
                 ),
               ),
-              child: Row(
+              child: Column(
                 children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      "https://img.freepik.com/foto-gratis/chico-guapo-seguro-posando-contra-pared-blanca_176420-32936.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1699401600&semt=ais",
-                    ),
-                    radius: 60,
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://img.freepik.com/foto-gratis/chico-guapo-seguro-posando-contra-pared-blanca_176420-32936.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1699401600&semt=ais",
+                        ),
+                        radius: 60,
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Federico Perez",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              "Developer",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white.withOpacity(0.6),
+                              ),
+                            ),
+                            Divider(),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.4),
+                                  borderRadius: BorderRadius.circular(5)),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column1("Folowers", "25"),
+                                  Column1("Likes", "254"),
+                                  Column1("Rating", "8.3")
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Federico Perez",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Chat",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(
+                              color: Color(0xffee9b00),
+                            ),
                           ),
                         ),
-                        Text(
-                          "Developer",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white.withOpacity(0.6),
+                      ),
+                      VerticalDivider(),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Follow"),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(
+                              0xff0a9396,
+                            ),
                           ),
                         ),
-                        Divider(),
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.4),
-                              borderRadius: BorderRadius.circular(5)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Column1("Folowers", "25"),
-                              Column1("Likes", "254"),
-                              Column1("Rating", "8.3")
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                    ],
                   )
                 ],
               ),
